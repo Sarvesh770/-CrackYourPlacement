d282e1072fd598f05a7bf152fd60d7ae0463fa05
Sarvesh770 <100180284+Sarvesh770@users.noreply.github.com>
2024-07-24 23:46:29 +0530

    Time: 3 ms (10.46%), Space: 7.4 MB (16.24%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string convertToTitle(int n) {
+        string s="";
+        while(n){
+            char c=(n-1)%26 + 'A';
+            s = c + s;
+            n = (n-1)/26;
+        }
+        return s;
+    }
+};
