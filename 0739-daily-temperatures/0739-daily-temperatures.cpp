c4fdac0ca2debea54af4c28ae316cc8d1b1ab3f1
Sarvesh770 <100180284+Sarvesh770@users.noreply.github.com>
2024-07-30 22:21:49 +0530

    Time: 138 ms (14.06%), Space: 101.3 MB (72.63%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& t) {
+        int len = t.size();
+        stack<int> s;
+        s.push(len - 1);
+        vector<int> res(len);
+        // looping from right to left, starting from the penultimate element
+        for (int i = len - 2, n; i >=0; --i) {
+            n = t[i];
+            // removing all the indexes stacked before if they are smaller
+            while (s.size() && n >= t[s.top()]) s.pop();
+            // if we still have something in the stack, we update res
+            if (s.size()) res[i] = s.top() - i;
+            // updating the stack
+            s.push(i);
+        }
+        return res;
+    }
+};
